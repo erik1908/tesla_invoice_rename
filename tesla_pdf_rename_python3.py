a1a6b4af992fdcb02a24e53bbdb9a65c829ec089
erik1908 <erikgrasman@icloud.com>
2021-01-11 23:24:26 +0100

    minor bugfix

--- tesla_pdf_rename_python3.py
+++ tesla_pdf_rename_python3.py
@@ -1,14 +1,14 @@
 import os, PyPDF2, re, io, shutil
 
 # parameter variables
-root_dir = r"/Users/erik/Documents/development/git/tesla_invoice_rename"
+root_dir = r"/Users/erik/Documents/development/git/tesla_invoice_rename/pdfs"
 rename_to = r"/Users/erik/Documents/development/git/tesla_invoice_rename/renamed"
 
 
 # function for renaming the pdfs based on text in the pdf
 def rename_pdfs(root_dir, rename_folder):
  # traverse down through the root directory to sub-directories
- for root, dirs, files in os.walk(root_dir):
+ for root, _, files in os.walk(root_dir):
   for filename in files:
    basename, extension = os.path.splitext(filename)
    # if a file is a pdf
